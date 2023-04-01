5401c40bd580b451da41f9886ea6a489e1f04563
Eva Li <evali@truckstop.com>
2023-03-31 22:44:50 -0500

    adds theme colors

---
+++ lib/presentation/theme/color_palette.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class ColorPalette {
+  const ColorPalette._();
+
+  static const Color lightGreen = Color(0xFFB1FBB8);
+  static const Color electricBlue = Color(0xFF8EECF5);
+  static const Color jordyBlue = Color(0xFFA3C4F3);
+  static const Color teaRose = Color(0xFFFFCFD2);
+  static const Color lemonChiffon = Color(0xFFFBF8CC);
+  static const Color aquamarine = Color(0xFF98F5E1);
+  static const Color nonPhotoBlue = Color(0xFF90DBF4);
+  static const Color mauve = Color(0xFFCFBAF0);
+  static const Color champagnePink = Color(0xFFFDE4CF);
+}
---
+++ lib/presentation/theme/theme_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'color_palette.dart';
+
+class ThemeColor {
+  const ThemeColor._();
+
+  static const Color mainGreen = ColorPalette.lightGreen;
+  static const Color mainBlue = ColorPalette.electricBlue;
+  static const Color pastelYellow = ColorPalette.lemonChiffon;
+  static const Color pastelBlue = ColorPalette.jordyBlue;
+  static const Color pastelPurple = ColorPalette.mauve;
+  static const Color coral = ColorPalette.teaRose;
+  static const Color aquamarine = ColorPalette.aquamarine;
+  static const Color skyBlue = ColorPalette.nonPhotoBlue;
+  static const Color champagne = ColorPalette.champagnePink;
+}
